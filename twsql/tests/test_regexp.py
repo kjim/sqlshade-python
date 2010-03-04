f185b5acadc1718528cb923a7fb23ba5d17ef090
Keiji Muraishi <keiji.muraishi@gmail.com>
2010-03-04 23:59:07 +0900

    temporary commit

--- twsql/tests/test_regexp.py
+++ twsql/tests/test_regexp.py
@@ -81,9 +81,9 @@ class RegexpPatternTest(unittest.TestCase):
                 \*/     # closing
                 
                 (       # phantom
-                  '([^\\]|(\\.))*?'   # string literal
+                  '(?:[^\\]|(\\.))*?'   # string literal
                   |
-                  \('([^\\]|(\\.))*?'\)   # in paren literal
+                  \( '(?:[^\\]|(\\.))*?' \)   # in paren literal
                   |
                   [^\s\n\r]+          # literal
                 )
