f42a5748d83452389adf45feb1d3dba461b7e21a
Keiji Muraishi <keiji.muraishi@gmail.com>
2010-03-04 18:04:49 +0900

    add control_comment_end pattern test

--- twsql/tests/test_regexp.py
+++ twsql/tests/test_regexp.py
@@ -16,22 +16,26 @@ class RegexpPatternTest(unittest.TestCase):
                 \*/   # closing
                 """
         reg = re.compile(pattern, re.S | re.X)
+
+        # for statement
         match = reg.match('/*#for item in :items*/followed text...')
         assert match is not None
         assert match.group(1) == 'for'
         assert match.group(2) == ' item in :items'
 
+        # embed statement
         match = reg.match('/*#embed :embed_item*/followed text...')
         assert match is not None
         assert match.group(1) == 'embed'
         assert match.group(2) == ' :embed_item'
 
-        match = reg.match('/*#if :boolean_item*/followed test...')
+        # if statement
+        match = reg.match('/*#if :boolean_item*/followed text...')
         assert match is not None
         assert match.group(1) == 'if'
         assert match.group(2) == ' :boolean_item'
 
-        # invalid patterns
+        # invalid values
         assert reg.match('/*for item in :items*/followed test...') is None
         assert reg.match('/*embed :item*/followed test...') is None
         assert reg.match('/*if :item*/followed test...') is None
@@ -40,3 +44,25 @@ class RegexpPatternTest(unittest.TestCase):
         assert reg.match('/*#for item in :ite:ms*/') is None
         assert reg.match('/*#for item in $items*/') is None
         assert reg.match('/*#for item in #items*/') is None
+
+    def test_control_comment_end_pattern(self):
+        pattern = r'/\*#/[\t ]*(.+?)[\t ]*\*/'
+        reg = re.compile(pattern)
+
+        # for statement
+        match = reg.match('/*#/for*/ followed text')
+        assert match is not None
+        assert match.group(1) == 'for'
+
+        # embed statement
+        match = reg.match('/*#/embed*/')
+        assert match is not None
+        assert match.group(1) == 'embed'
+
+        # if statement
+        match = reg.match('/*#/if*/')
+        assert match is not None
+        assert match.group(1) == 'if'
+
+        # invalid values
+        assert reg.match('/*#for*/') is None
