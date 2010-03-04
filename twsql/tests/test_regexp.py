67505f531412dd992199013c693b224b3d6a4d74
Keiji Muraishi <keiji.muraishi@gmail.com>
2010-03-04 17:43:08 +0900

    contains space

--- twsql/tests/test_regexp.py
+++ twsql/tests/test_regexp.py
@@ -9,9 +9,7 @@ class RegexpPatternTest(unittest.TestCase):
                 
                 ([\w\.\:]+)   # keyword
 
-                \s*     # whitespace
-                
-                ((?:\s+|:\w+|\w+)*)  # text
+                ((?:\s+:?\w+)*)  # text
                 
                 \s*     # more whitespace
                 
@@ -21,14 +19,24 @@ class RegexpPatternTest(unittest.TestCase):
         match = reg.match('/*#for item in :items*/followed text...')
         assert match is not None
         assert match.group(1) == 'for'
-        assert match.group(2) == 'item in :items'
+        assert match.group(2) == ' item in :items'
 
         match = reg.match('/*#embed :embed_item*/followed text...')
         assert match is not None
         assert match.group(1) == 'embed'
-        assert match.group(2) == ':embed_item'
+        assert match.group(2) == ' :embed_item'
 
-        match = reg.match('/*#if :boolean_item*/')
+        match = reg.match('/*#if :boolean_item*/followed test...')
         assert match is not None
         assert match.group(1) == 'if'
-        assert match.group(2) == ':boolean_item'
+        assert match.group(2) == ' :boolean_item'
+
+        # invalid patterns
+        assert reg.match('/*for item in :items*/followed test...') is None
+        assert reg.match('/*embed :item*/followed test...') is None
+        assert reg.match('/*if :item*/followed test...') is None
+
+        assert reg.match('/*#for item in ite:ms*/') is None
+        assert reg.match('/*#for item in :ite:ms*/') is None
+        assert reg.match('/*#for item in $items*/') is None
+        assert reg.match('/*#for item in #items*/') is None
