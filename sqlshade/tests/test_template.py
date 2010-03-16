a7e294d345a7da49f86099f6ed58285e5861dd9c
Keiji Muraishi <keiji.muraishi@gmail.com>
2010-03-17 00:07:06 +0900

    add test for parameter_format

--- sqlshade/tests/test_template.py
+++ sqlshade/tests/test_template.py
@@ -7,42 +7,34 @@ from sqlshade.template import Template
 class SubstituteAnyCaseTest(unittest.TestCase):
 
     def test_simple_substitute(self):
-        plain = """SELECT * FROM t_member WHERE name = /*:nickname*/'kjim'"""
-        template = Template(plain)
-        query, bound_variables = template.render(nickname='keiji')
+        plain_query = """SELECT * FROM t_member WHERE name = /*:nickname*/'kjim'"""
+        parameters = dict(nickname='keiji')
+        template = Template(plain_query, parameter_format=list)
+        query, bound_variables = template.render(**parameters)
         assert query == "SELECT * FROM t_member WHERE name = ?"
         assert bound_variables == ['keiji']
 
-        template = Template(plain, parameter_format='dict')
-        query, bound_variables = template.render(nickname='keiji')
+        template = Template(plain_query, parameter_format=dict)
+        query, bound_variables = template.render(**parameters)
         assert query == "SELECT * FROM t_member WHERE name = :nickname"
-        assert bound_variables == {'nickname': 'keiji'}
+        assert bound_variables == parameters
 
     def test_substitute_scalar_variables(self):
-        template = Template("""
-            SELECT
-                *
-            FROM
-                t_member
+        plain_query = """SELECT * FROM t_member
             WHERE TRUE
                 AND t_member.age = /*:age*/1000
                 AND t_member.nickname = /*:nickname*/'my nickname is holder'
                 AND t_member.updated_at = /*:updated_at*/CURRENT_TIMESTAMP
                 AND t_member.created_at <= /*:created_at*/now()
             ;
-        """)
+        """
         created_at, updated_at = datetime.now(), datetime.now()
-        query, bound_variables = template.render(
-            age=25,
-            nickname='kjim',
-            updated_at=updated_at,
-            created_at=created_at
-        )
-        assert query == """
-            SELECT
-                *
-            FROM
-                t_member
+        parameters = dict(age=25, nickname='kjim', updated_at=updated_at, created_at=created_at)
+
+        # format: list
+        template = Template(plain_query, parameter_format=list)
+        query, bound_variables = template.render(**parameters)
+        assert query == """SELECT * FROM t_member
             WHERE TRUE
                 AND t_member.age = ?
                 AND t_member.nickname = ?
@@ -52,27 +44,32 @@ class SubstituteAnyCaseTest(unittest.TestCase):
         """
         assert bound_variables == [25, 'kjim', updated_at, created_at]
 
+        # format: dict
+        template = Template(plain_query, parameter_format=dict)
+        query, bound_variables = template.render(**parameters)
+        assert query == """SELECT * FROM t_member
+            WHERE TRUE
+                AND t_member.age = :age
+                AND t_member.nickname = :nickname
+                AND t_member.updated_at = :updated_at
+                AND t_member.created_at <= :created_at
+            ;
+        """
+        assert bound_variables == parameters
+
     def test_substitute_container_variables(self):
-        template = Template("""
-            SELECT
-                *
-            FROM
-                t_member
+        plain_query = """SELECT * FROM t_member
             WHERE TRUE
                 AND t_member.member_id IN /*:member_id*/(100, 200)
                 AND t_member.nickname LIKE /*:nickname*/'%kjim%'
                 AND t_member.sex IN /*:sex*/('male', 'female')
-        """)
-        query, bound_variables = template.render(
-            member_id=[3845, 295, 1, 637, 221, 357],
-            nickname='%keiji%',
-            sex=('male', 'female', 'other')
-        )
-        assert query == """
-            SELECT
-                *
-            FROM
-                t_member
+        """
+        parameters = dict(member_id=[3845, 295, 1, 637, 221, 357], nickname='%keiji%', sex=('male', 'female', 'other'))
+
+        # format: list
+        template = Template(plain_query, parameter_format=list)
+        query, bound_variables = template.render(**parameters)
+        assert query == """SELECT * FROM t_member
             WHERE TRUE
                 AND t_member.member_id IN (?, ?, ?, ?, ?, ?)
                 AND t_member.nickname LIKE ?
@@ -80,126 +77,75 @@ class SubstituteAnyCaseTest(unittest.TestCase):
         """
         assert bound_variables == [3845, 295, 1, 637, 221, 357, '%keiji%', 'male', 'female', 'other']
 
+        # format: dict
+        template = Template(plain_query, parameter_format=dict)
+        query, bound_variables = template.render(**parameters)
+        assert query == """SELECT * FROM t_member
+            WHERE TRUE
+                AND t_member.member_id IN :member_id
+                AND t_member.nickname LIKE :nickname
+                AND t_member.sex IN :sex
+        """
+        assert bound_variables == parameters
+
     def test_substitute_same_keys(self):
-        template = Template("""
-            select
-                *
-            from
+        plain_query = """SELECT *
+            FROM
                 t_member
-                inner join t_member_activation
-                  on (t_member_activation.member_id = t_member.member_id)
-            where true
-                and t_member.satus = /*:status_activated*/0
-                and t_member_activation.status = /*:status_activated*/0
-        """)
-        query, bound_variables = template.render(status_activated=1)
-        assert query == """
-            select
-                *
-            from
+                INNER JOIN t_member_activation
+                  ON (t_member_activation.member_id = t_member.member_id)
+            WHERE TRUE
+                AND t_member.satus = /*:status_activated*/0
+                AND t_member_activation.status = /*:status_activated*/0
+            ;
+        """
+        parameters = dict(status_activated=1)
+
+        # format: list
+        template = Template(plain_query, parameter_format=list)
+        query, bound_variables = template.render(**parameters)
+        assert query == """SELECT *
+            FROM
                 t_member
-                inner join t_member_activation
-                  on (t_member_activation.member_id = t_member.member_id)
-            where true
-                and t_member.satus = ?
-                and t_member_activation.status = ?
+                INNER JOIN t_member_activation
+                  ON (t_member_activation.member_id = t_member.member_id)
+            WHERE TRUE
+                AND t_member.satus = ?
+                AND t_member_activation.status = ?
+            ;
         """
         assert bound_variables == [1, 1]
 
-    def test_raise_if_has_nofeed_substitute_variables(self):
-        template = Template("""
-            select
-                *
-            from
+        # format: dict
+        template = Template(plain_query, parameter_format=dict)
+        query, bound_variables = template.render(**parameters)
+        assert query == """SELECT *
+            FROM
                 t_member
-            where true
-                and t_member.member_id in /*:member_ids*/(100, 200, 300, 400)
-                and t_member.nickname = /*:nickname*/'kjim'
-        """)
+                INNER JOIN t_member_activation
+                  ON (t_member_activation.member_id = t_member.member_id)
+            WHERE TRUE
+                AND t_member.satus = :status_activated
+                AND t_member_activation.status = :status_activated
+            ;
+        """
+        assert bound_variables == parameters
+
+    def test_raise_if_has_nofeed_substitute_variables(self):
+        template = Template("""SELECT * FROM t_member
+            WHERE TRUE
+                AND t_member.member_id IN /*:member_ids*/(100, 200, 300, 400)
+                AND t_member.nickname = /*:nickname*/'kjim'
+        """, parameter_format=list)
         self.assertRaises(exc.RuntimeError, template.render)
         self.assertRaises(exc.RuntimeError, template.render, nickname='keiji')
 
     def test_raise_if_feed_empty_substittue_list_variables(self):
-        template = Template("""
-            select
-                *
-            from
-                t_member
-            where true
-                and t_member.member_id in /*:member_ids*/(100, 200, 300, 400)
-        """)
+        template = Template("""SELECT * FROM t_member
+            WHERE TRUE
+                AND t_member.member_id IN /*:member_ids*/(100, 200, 300, 400)
+        """, parameter_format=list)
         self.assertRaises(exc.RuntimeError, template.render, member_ids=[])
-
-    def test_dynamic_select_column_query_template(self):
-        template = Template("""
-            SELECT
-                t_template.id
-                /*#for column in :table_columns*/
-                , /*#embed :column*/t_template.engine/*#endembed*/
-                /*#endfor*/
-            FROM
-                t_template
-            WHERE
-                FALSE
-                /*#for cond in :cond_engines*/
-                OR (t_template.engine = /*:cond.engine*/'mako' AND t_template.feature IN /*:cond.features*/('fast'))
-                /*#endfor*/
-            ;
-        """)
-        query, bound_variables = template.render(
-            table_columns=[
-                't_template.engine',
-                't_template.feature',
-            ],
-            cond_engines=[
-                { 'engine': 'Mako', 'features': ['fast', 'non-XML', 'module'] },
-                { 'engine': 'Jinja2', 'features': ['fast', 'non-XML'] },
-                { 'engine': 'Genshi', 'features': ['XML'] },
-            ]
-        )
-        assert ", t_template.engine" in query
-        assert ", t_template.feature" in query
-        assert "OR (t_template.engine = ? AND t_template.feature IN (?, ?, ?))" in query
-        assert "OR (t_template.engine = ? AND t_template.feature IN (?, ?))" in query
-        assert "OR (t_template.engine = ? AND t_template.feature IN (?))" in query
-        assert bound_variables == [
-            'Mako',
-            'fast',
-            'non-XML',
-            'module',
-            'Jinja2',
-            'fast',
-            'non-XML',
-            'Genshi',
-            'XML',
-        ]
-
-    def test_eval_tricks(self):
-        template = Template("""
-            SELECT
-                *
-            FROM
-                t_template
-            WHERE TRUE
-                /*#eval :cond_trick_literal*/
-                AND (t_template.engine = 'Mako')
-                /*#endeval*/
-            ;
-        """)
-        query, bound_variables = template.render(
-            cond_trick_literal="""AND (t_template.engine = /*:cond_engine*/'Mako')""",
-            cond_engine='Genshi'
-        )
-        assert query == """
-            SELECT
-                *
-            FROM
-                t_template
-            WHERE TRUE
-                AND (t_template.engine = ?)
-            ;
-        """
-        assert bound_variables == ['Genshi']
 
 class EmbedAnyCaseTest(unittest.TestCase):
 
